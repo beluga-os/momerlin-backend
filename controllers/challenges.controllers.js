9a0f273f5111a010812418a88232ca6421c8d7c4
mithranjith <mithran.ranjith@gmail.com>
2021-11-13 16:14:46 +0530

    Fixed an issue in gwei calculation while join in a challenge

--- controllers/challenges.controllers.js
+++ controllers/challenges.controllers.js
@@ -273,7 +273,7 @@ const joinChallenge = async function (req, res) {
                                     return ReE(res, error, 400)
                                 }
 
-                                user.gwei = new BigNumber(String(parseInt(challenge.wage))).minus(new BigNumber(user.gwei)).toString(10)
+                                user.gwei = new BigNumber(user.gwei).minus(new BigNumber(challenge.wage)).toString(10)
 
                                 user.eth = new BigNumber(new BigNumber(String(parseInt(user.gwei)))).div(new BigNumber('1000000000000000000'), 10).toString(10)
 
